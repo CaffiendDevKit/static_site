55e1e4e99dedeb5d6cbafa0334d57c58b6ec8228
CaffiendDevKit <github.scoop462@passfwd.com>
2025-01-25 19:49:59 +0800

    Added 3 test cases for HTMLNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -2,7 +2,7 @@
 
 class HTMLNode():
     def __init__(
-            self=None,
+            self,
             tag=None, # An HTMLNode without a tag will just render as raw text
             value=None, # An HTMLNode without a value will be assumed to have children
             children=None, # An HTMLNode without children will be assumed to have a value
@@ -17,32 +17,42 @@ class HTMLNode():
 
     def props_to_html(self):
         return_str=""
-        for key in pops.keys():
-            return_str = f"{key}=\"{props[key]}\" "
+        for key in self.props.keys():
+            return_str += f" {key}=\"{self.props[key]}\""
 
         return return_str
 
     def __repr__(self):
-        if tag:
-            tag_str = f"tag is: {tag}"
+        if self.tag:
+            tag_str = f"tag is: {self.tag}"
         else:
             tag_str = "The tag is empty"
 
-        if value:
-            value_str = f"value is {value}"
+        if self.value:
+            value_str = f"value is {self.value}"
         else:
             value_str = "Value is empty"
 
-        if children:
-            child_str = f"children is {children}"
+        if self.children:
+            child_str = f"children is {self.children}"
         else:
             child_str = "Children is empty"
 
-        if props:
+        if self.props:
             props_str = "The props dict contains"
-            for key in pops.keys():
-                props_str = f"\nprop: {key}  -  attribute: {props[key]}"
+            for key in self.props.keys():
+                props_str += f"\nprop: {key}  -  attribute: {self.props[key]}"
         else:
             props_str = "The props dict is empty"
 
         return "\n".join([tag_str, value_str, child_str, props_str])
+    
+class LeafNode():
+    def __init__(
+            self,
+            tag, # An HTMLNode without a tag will just render as raw text
+            value, # An HTMLNode without a value will be assumed to have children
+            props=None): # An HTMLNode without props simply won't have any attributes
+        self.tag = tag
+        self.value = value
+        self.props = props
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,25 +3,36 @@ import unittest
 from htmlnode import HTMLNode
 
 class TesHTMLNode(unittest.TestCase):
-    def test_eq(self):
-        node = HTMLNode(value="")
-        node2 = HTMLNode("This is a text node", TextType.BOLD)
-        self.assertEqual(node,node2)
+    def test_repr1(self):
+        value = "This is an example"
+        test_prop1 = {"target": "_blank"}
+        node = HTMLNode(value=value, props=test_prop1)
 
-    def test_not_eq_type(self):
-        node = HTMLNode("This is a text node", TextType.BOLD)
-        node2 = HTMLNode("This is a text node", TextType.NORMAL)
-        self.assertNotEqual(node, node2)
+        expected_tag = "The tag is empty"
+        expected_value = "value is This is an example"
+        expected_children = "Children is empty"
+        expected_props = "The props dict contains\nprop: target  -  attribute: _blank"
+        expected_out = "\n".join([expected_tag, expected_value, expected_children, expected_props])
 
-    def test_not_eq_text(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a different text node", TextType.BOLD)
-        self.assertNotEqual(node, node2)
+        print(f"node is of type: type(node_out)")
+        self.assertEqual(str(node), expected_out)
 
-    def test_not_eq_url(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD, "www.website.com")
-        self.assertNotEqual(node, node2)
+    def test_to_html1(self):
+        value = "This is an example"
+        test_prop1 = {"target": "_blank"}
+        node = HTMLNode(value=value, props=test_prop1)
+
+        expected_out = " target=\"_blank\""
+        self.assertEqual(node.props_to_html(), expected_out)
+
+    def test_to_html2(self):
+        value = "This is an example"
+        test_prop2 = {"href": "https://www.boot.dev", "target": "_blank"}
+        node = HTMLNode(value=value, props=test_prop2)
+
+        expected_out = " href=\"https://www.boot.dev\" target=\"_blank\""
+        self.assertEqual(node.props_to_html(), expected_out)
+
 
 if __name__ == "__main__":
     unittest.main()
